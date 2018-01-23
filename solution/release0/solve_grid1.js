f0fdfbaea6f9b1ae17b0e645488464a96f893f3f
School of Accelerated Learning <soal@Schools-Mac-mini-2.local>
2018-01-23 20:17:28 +0530

    added code to check row colum and grid

---
+++ solution/release0/solve_grid1.js
@@ -0,0 +1,102 @@
+var board = [
+      [0,9,0,0,0,0,0,0,6],
+      [0,0,0,9,6,0,4,8,5],
+      [0,0,0,5,8,1,0,0,0],
+      [0,0,4,0,0,0,0,0,0],
+      [5,1,7,2,0,0,9,0,0],
+      [6,0,2,0,0,0,3,7,0],
+      [1,0,0,8,0,4,0,2,0],
+      [7,0,6,0,0,0,8,1,0],
+      [3,0,0,0,9,0,0,0,0]
+    ];
+     console.log(board);
+
+
+
+function makeInteger(){
+   	return board.split('\n').map(function(row){
+     			return row.split('').map(function(num){
+     				return +num;
+     			});
+     });
+
+   }  ;
+
+
+function saveEmptyPositions(){
+	// Create an array to save the positions
+	var saveEmptyPositions = [];
+
+	// Check every square in the puzzle for a zero
+	for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      // If a zero is found, save that position
+      if(board[i][j] === 0) {
+        emptyPositions.push([i, j]);
+      }
+    }
+  }
+}
+
+  // Return the positions
+  return emptyPositions;
+};
+
+function checkrow(board, row, value){
+	// Iterate through every value in the row
+  for(var i = 0; i < board[row].length; i++) {
+    // If a match is found, return false
+    if(board[row][i] === value) {
+      return false;
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+}
+
+function checkColumn(board , row , value){
+	// Iterate through each value in the column
+  for(var i = 0; i < board.length; i++) {
+    // If a match is found, return false
+    if(board[i][column] === value) {
+      return false;
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+
+function check3X3Square(){
+	// Save the upper left corner
+  var columnCorner = 0,
+      rowCorner = 0,
+      squareSize = 3;
+
+  // Find the left-most column
+  while(column >= columnCorner + squareSize) {
+    columnCorner += squareSize;
+  }
+
+  // Find the upper-most row
+  while(row >= rowCorner + squareSize) {
+    rowCorner += squareSize;
+  }
+
+  // Iterate through each row
+  for(var i = rowCorner; i < rowCorner + squareSize; i++) {
+    // Iterate through each column
+    for(var j = columnCorner; j < columnCorner + squareSize; j++) {
+      // Return false if a match is found
+      if(board[i][j] === value) {        
+        return false;
+      }
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+}
+
+
+
